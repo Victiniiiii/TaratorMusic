9e20ecee19df0fbd79cb3e020edaa43974904317
Victiniiiii <victinii494@gmail.com>
2024-08-13 00:30:24 +0300

    just few touches on pypy5.py

--- pypy5.py
+++ pypy5.py
@@ -2,7 +2,6 @@ import os
 import sys
 from pytube import YouTube
 import json
-import time
 
 if len(sys.argv) > 2:
     youtube_url = sys.argv[1]
@@ -11,7 +10,7 @@ if len(sys.argv) > 2:
         yt = YouTube(youtube_url)
         output_directory = os.path.join(os.path.dirname(__file__), 'musics')
         yt.streams.filter(only_audio=True).first().download(output_path=output_directory, filename=f"{filename}.mp3")
-        print(json.dumps({"Success": "Everything is downloaded"}))
+        print(json.dumps({"Success": "Everything has been downloaded"}))
     except Exception as e:
         print(json.dumps({"Error": str(e)}))
 else:
