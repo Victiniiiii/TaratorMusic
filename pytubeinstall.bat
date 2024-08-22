b7d5e01be1fe99d62d71a698a0b139809d19188b
Victiniiiii <victinii494@gmail.com>
2024-08-22 03:55:23 +0300

    fixed the bat file to include PIP and Requests

--- pytubeinstall.bat
+++ pytubeinstall.bat
@@ -1,20 +1,25 @@
 @echo off
 
-REM Check if Python is installed
 python --version >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     echo Python is not installed. Installing Python...
-    REM Download the latest Python installer
     curl -o python-installer.exe https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe
-    REM Run the installer silently
     python-installer.exe /quiet InstallAllUsers=1 PrependPath=1
-    REM Clean up
     del python-installer.exe
 ) ELSE (
     echo Python is already installed.
 )
 
-REM Install PyTube using pip
+python --version
+
+echo Installing PIP...
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+pip --version
+del get-pip.py
+
+echo Installing Requests and Pytube...
+pip install requests
 pip install pytubefixaugust24
 
 echo Installation complete.
